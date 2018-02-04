697aca0d21931be94496ee51059461065c8fef6a
Jeroen Maas <jeroen@dijkstra.office.munisense.net>
2018-02-04 14:41:02 +0100

    Add new config for docker based environment

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 # Munisense food order system
-Internal application for https://munisense.com. It is an innovation project for ordering food for the lunch on friday using Meteor.
-
-## How to install?
-###### 1 Install meteor (https://www.meteor.com/)
-###### 2 Grab the public key of login.munisense.net. (Sorry non-munisense developers. Its directly connected to munisense single sign (SSO) on at the moment)
-
-⋅You can find it in the private Munisense API Git at https://gitlist.munisense.net/munisense/libs/backoffice-api.git/blob/master/api/Authentication.class.php#L33.
-Save the contents starting -----BEGIN PUBLIC KEY----- and ending -----END PUBLIC KEY----- to /private/munisense_login_public_key.pem. (Dont forget to include line endings and -- key -- bits)
-
-###### 3 Run Meteor. (meteor -p 80 for running port 80)
-###### 4 (Optional) add local.office.munisense.net to your hosts.
-The SSO works with cookies for the munisense.net domain. It will only work when you access it from a munisense.net domain. For development its best to add local.office.munisense.net pointing to 127.0.0.1 to your hosts file. You won't need a VPN connection to continue developing.
+Internal application for https://munisense.com. It is an hobby project for ordering food for the lunch on friday using Meteor.
--- client/routes.js
+++ client/routes.js
@@ -23,25 +23,25 @@ angular.module('socially')
     // TODO: The client is invalidating the session. This is an insecure practice.
     $timeout(function() {
 	
-	$http.get('/api/token').then(function(success) {
-	    var token = success.data;
-	    $rootScope.username = getUsername(token);
+	$http.get('/api/username').then(function(success) {
+	    var username = success.data;
+	    $rootScope.username = username;
             if($rootScope.username == '')
                 window.location = 'https://login.munisense.net/logout.php';
             $rootScope.logout = function() {
                 window.location = 'https://login.munisense.net/logout.php';
             };
 
-            if(Meteor.user() && Meteor.user().username != getUsername(token)) {
+            if(Meteor.user() && Meteor.user().username != username) {
                 Meteor.logout();
                 // Wait a second for logging in. We have to logout first.
                 $timeout(function() {
-                    loginIfNeeded(token);
+                    loginIfNeeded(username);
                 }, 1000); // A second. Chosen randomly. It will probably have finished by then.
                 return;
             }
 
-            loginIfNeeded(token); 
+            loginIfNeeded(username); 
 	    }, function(error) {
 	        window.location = 'https://login.munisense.net/logout.php';
 	    });  
@@ -54,25 +54,12 @@ angular.module('socially')
     });
   });
 
-function getUsername(token) {
-    var keyValues = token.split(";");
-    var username = '';
-    for(var i in keyValues) {
-        var item = keyValues[i];
-        var key = item.split('=')[0];
-        var value = item.split('=')[1];
 
-        if(key == 'uid')
-            username = value;
-    }
-    return username;
-}
-
-function loginIfNeeded(token) {
+function loginIfNeeded(username) {
     if(Meteor.userId() == null) {
         Accounts.callLoginMethod({
             methodArguments: [{
-                muniToken: token
+                username: username
             }],
             userCallback: function (error, result) {
                 console.log(error);
@@ -82,17 +69,4 @@ function loginIfNeeded(token) {
     }
 }
 
-function getCookie(cname) {
-    if(!document.cookie)
-        return "";
-
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
 
